edc91038639fdffe3c4a3522f76ff1eda2dd0c66
mammarelm7 <mammarelm7@students.rowan.edu>
2017-09-16 14:55:00 -0400

    added comments

--- Assignment/math.c
+++ Assignment/math.c
@@ -1,23 +1,30 @@
+/*
+Matt Mammarelli
+9/16/17
+ECE 09342-2
+*/
+
 #include <stdio.h>
 #include "math.h"
 
-int result = 0; //result
+//needs stdio.h for printf. needs math.h for math(int,int,char) function
+
+int result = 0; //result of math operation
 
 
 int main()
 {
-	int num1=5;
-	int num2=2;
-	int operator='%';
+	int num1=5; //operand 1
+	int num2=2; //operand 2
+	int operator='%'; 
 	
-	
-	
-	printf ("Answer is: %i", math(num1,num2,operator));
+	printf ("Answer is: %i", math(num1,num2,operator)); //performs math operation and prints result
 	
 	return 0;
 }
-
+//implementation of math function in math.h
 int math(int num1, int num2, char Operator){
+	//checks operator and sets result for each corresponding math operation
 	switch(Operator){
 		case '+':
 			result = num1+num2;
@@ -35,7 +42,7 @@ int math(int num1, int num2, char Operator){
 			result = num1%num2;
 			break;
 		default:
-			printf("Invalid operator");	
+			printf("Invalid operator");	//if operator not one of the above, prints invalid operator
 	}
 	
 	return result;
