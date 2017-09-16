3ae88bf96f5075969d99a219868b5d5dbd03b043
mammarelm7 <mammarelm7@students.rowan.edu>
2017-09-16 14:18:10 -0400

    added math.c

---
+++ Assignment/math.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "math.h"
+
+int result = 0;
+
+
+int main()
+{
+	int num1=5;
+	int num2=2;
+	int operator='%';
+	
+	
+	
+	printf ("Answer is: %i", math(num1,num2,operator));
+	
+	return 0;
+}
+
+int math(int num1, int num2, char Operator){
+	switch(Operator){
+		case '+':
+			result = num1+num2;
+			break;
+		case '-':
+			result = num1-num2;
+			break;
+		case '*':
+			result = num1*num2;
+			break;
+		case '/':
+			result = num1/num2;
+			break;
+		case '%':
+			result = num1%num2;
+			break;
+		default:
+			printf("Invalid operator");	
+	}
+	
+	return result;
+	
+} 
